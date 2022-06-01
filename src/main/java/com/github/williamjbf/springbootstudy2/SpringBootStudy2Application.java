b221407bcbf7540a9c9946b2da275960169e705b
williamjbf <williamjbf@gmail.com>
2022-06-01 18:40:30 -0300

    startup project

---
+++ src/main/java/com/github/williamjbf/springbootstudy2/SpringBootStudy2Application.java
@@ -0,0 +1,13 @@
+package com.github.williamjbf.springbootstudy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudy2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/williamjbf/springbootstudy2/SpringBootStudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.williamjbf.springbootstudy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudy2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
