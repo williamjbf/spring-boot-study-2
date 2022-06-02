0cc50e9977351cf76072415e3e08f684f1dd1625
williamjbf <williamjbf@gmail.com>
2022-06-01 22:30:59 -0300

    created a table to list the created customers

--- src/main/java/com/github/williamjbf/springbootstudy2/Controller/CustomerController.java
+++ src/main/java/com/github/williamjbf/springbootstudy2/Controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CustomerController {
@@ -24,5 +25,12 @@ public class CustomerController {
         customerRepository.save(customer);
 
         return "registration/registrationcustomer";
+    }
+    @RequestMapping(method = RequestMethod.GET, value = "/listcustomers")
+    public ModelAndView listAllCustomers(){
+        ModelAndView modelAndView = new ModelAndView("registration/registrationcustomer");
+        Iterable<Customer> customers = customerRepository.findAll();
+        modelAndView.addObject("customers",customers);
+        return modelAndView;
     }
 }
